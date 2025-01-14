61057b89b29e465bd5da369848907a5ffe9907da
Saygel <jmicu@myges.fr>
2025-01-14 10:51:30 +0100

    add asm5

--- asm05/asm05.s
+++ asm05/asm05.s
@@ -1,90 +1,26 @@
-section .bss
-    num1 resq 1
-    num2 resq 1
-    result resb 21
-
 section .text
-    global _start
+global _start
 
 _start:
-    mov rdi, [rsp]
-    cmp rdi, 3
-    jl insufficient_args
+    ; Vérifier si au moins un argument a été passé
+    mov rax, [rsp]          ; nombre d'arguments dans argc
+    cmp rax, 1              ; argc doit être > 1 pour avoir au moins un argument
+    jle no_argument         ; sauter si aucun argument n'est passé
 
-    mov rsi, [rsp+16]
-    call atoi
-    mov [num1], rax
+    ; Préparer l'appel système write pour afficher l'argument
+    mov rax, 1              ; syscall number for write
+    mov rdi, 1              ; file descriptor 1 (stdout)
+    mov rsi, [rsp + 16]     ; adresse du premier argument (argv[1])
+    mov rdx, [rsp + 24]     ; taille du premier argument (argv[1] length)
+    syscall                 ; effectuer l'appel système
 
-    mov rsi, [rsp+24]
-    call atoi
-    mov [num2], rax
+    ; Terminer le programme avec le code de retour 0
+    xor rdi, rdi            ; code de retour 0
+    mov rax, 60             ; syscall number for exit
+    syscall                 ; effectuer l'appel système
 
-    mov rax, [num1]
-    add rax, [num2]
-
-    mov rdi, result
-    call itoa
-
-    lea rsi, [result]
-    mov rax, 1
-    mov rdi, 1
-    mov rdx, 21
-    syscall
-
-    xor rdi, rdi
-    mov rax, 60
-    syscall
-
-insufficient_args:
-    mov rdi, 1
-    mov rax, 60
-    syscall
-
-atoi:
-    xor rax, rax
-    xor rbx, rbx
-    mov rcx, rsi
-    cmp byte [rcx], '-'
-    jne .loop
-    inc rsi
-    mov bl, 1
-.loop:
-    movzx rcx, byte [rsi]
-    test rcx, rcx
-    jz .done
-    sub rcx, '0'
-    imul rax, rax, 10
-    add rax, rcx
-    inc rsi
-    jmp .loop
-.done:
-    test bl, bl
-    jz .positive
-    neg rax
-.positive:
-    ret
-
-itoa:
-    mov rbx, 10
-    xor rcx, rcx
-    test rax, rax
-    jns .positive_number
-    neg rax
-    mov cl, '-'
-.positive_number:
-    lea rdi, [rdi + 20]
-    mov byte [rdi], 0
-.convert:
-    dec rdi
-    xor rdx, rdx
-    div rbx
-    add dl, '0'
-    mov [rdi], dl
-    test rax, rax
-    jnz .convert
-    test rcx, rcx
-    jz .done
-    dec rdi
-    mov [rdi], cl
-.done:
-    ret
+no_argument:
+    ; Terminer le programme avec le code de retour 1 s'il n'y a pas d'argument
+    mov rdi, 1              ; code de retour 1
+    mov rax, 60             ; syscall number for exit
+    syscall                 ; effectuer l'appel système
