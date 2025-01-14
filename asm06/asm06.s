f18db1f5d3af5c15d81f1bcd9cdd8d602fe9a9be
Saygel <jmicu@myges.fr>
2025-01-14 11:53:36 +0100

    asm06 add

--- asm06/asm06.s
+++ asm06/asm06.s
@@ -1,67 +1,98 @@
 global _start
 
-section .data
-    message db 'Entrez un nombre: ', 0xA
-
 section .bss
-    nombre resb 32
+buffer resb 32
 
 section .text
 
 _start:
+    mov rax, [rsp]
+    cmp rax, 3
+    jl no_args
+
+    mov rdi, [rsp+16]
+    call str_to_int
+    mov r8, rax
+
+    mov rdi, [rsp+24]
+    call str_to_int
+    add r8, rax
+    mov rdi, r8
+    call int_to_str
+
+    mov rdx, rax
     mov rax, 1
     mov rdi, 1
-    mov rsi, message
-    mov rdx, 17
+    mov rsi, buffer
     syscall
 
-    mov rax, 0
-    mov rdi, 0
-    mov rsi, nombre
-    mov rdx, 32
-    syscall
-
-    sub rax, 1
-    mov rcx, rax
-    lea rsi, [nombre]
-
+    mov rax, 60
     xor rdi, rdi
-
-convert_decimal:
-    xor rax, rax
-    movzx rax, byte [rsi]
-    sub rax, '0'
-    cmp rax, 10
-    jae est_premier
-    imul rdi, rdi, 10
-    add rdi, rax
-    inc rsi
-    loop convert_decimal
-
-est_premier:
-    cmp rdi, 2
-    jb non_premier
-    je premier
-    mov rcx, rdi
-    shr rcx, 1
-    mov rsi, 2
-
-test_prime:
-    mov rax, rdi
-    xor rdx, rdx
-    div rsi
-    cmp rdx, 0
-    je non_premier
-    inc rsi
-    cmp rsi, rcx
-    jbe test_prime
-
-premier:
-    mov rax, 60 
-    mov rdi, 0
     syscall
 
-non_premier:
+str_to_int:
+    xor rax, rax
+    xor rcx, rcx
+    xor rdx, rdx
+
+    mov dl, [rdi]
+    cmp dl, '-'
+    jne parse_loop
+    inc rdi
+    mov rcx, 1
+
+parse_loop:
+    mov dl, [rdi]
+    cmp dl, 0
+    je str_done
+    sub dl, '0'
+    imul rax, rax, 10
+    add rax, rdx
+    inc rdi
+    jmp parse_loop
+
+str_done:
+    cmp rcx, 1
+    jne str_end
+    neg rax
+
+str_end:
+    ret
+
+int_to_str:
+    mov rax, rdi
+    mov rbx, buffer + 31
+    mov byte [rbx], 10
+    dec rbx
+
+    xor rcx, rcx
+    test rax, rax
+    jge positive
+
+    neg rax
+    mov rcx, 1
+
+positive:
+itoa_loop:
+    xor rdx, rdx
+    mov r8, 10
+    div r8
+    add rdx, '0'
+    mov [rbx], dl
+    dec rbx
+    test rax, rax
+    jne itoa_loop
+
+    cmp rcx, 1
+    jne itoa_done
+    mov byte [rbx], '-'
+    dec rbx
+
+itoa_done:
+    lea rax, [rbx+1]
+    ret
+
+no_args:
     mov rax, 60
     mov rdi, 1
     syscall
