f87721ca185d4f514ecacbabaed85113b46624dc
Saygel <jmicu@myges.fr>
2025-01-14 10:59:47 +0100

    asm05 test

--- asm05/asm05.s
+++ asm05/asm05.s
@@ -2,38 +2,32 @@ section .text
 global _start
 
 _start:
-    mov rax, [rsp]        
-    cmp rax, 1             
-    jle no_argument      
+    mov rax, [rsp]
+    cmp rax, 1
+    jle no_argument
 
+    mov rsi, [rsp + 16]
+    call string_length
 
-    mov rsi, [rsp + 16]    
+    mov rax, 1
+    mov rdi, 1
+    syscall
 
-
-    call string_length    
-
- 
-    mov rax, 1           
-    mov rdi, 1            
-    mov rdx, rax        
-    syscall             
-
-
-    xor rdi, rdi     
-    mov rax, 60         
-    syscall               
+    xor rdi, rdi
+    mov rax, 60
+    syscall
 
 no_argument:
-    mov rdi, 1             
-    mov rax, 60            
-    syscall                
+    mov rdi, 1
+    mov rax, 60
+    syscall
 
 string_length:
-    xor rax, rax           
+    xor rax, rax
 count_loop:
-    cmp byte [rsi + rax], 0 
-    je end_count           
-    inc rax                
-    jmp count_loop         
+    cmp byte [rsi + rax], 0
+    je end_count
+    inc rax
+    jmp count_loop
 end_count:
-    ret                    
+    ret
