019c0d788d1c65e5ee296ff5133e01f0fdc070da
Saygel <jmicu@myges.fr>
2025-01-14 11:06:30 +0100

    final 05

--- asm05/asm05.s
+++ asm05/asm05.s
@@ -2,36 +2,33 @@ section .text
 global _start
 
 _start:
-    mov rax, [rsp]          ; Charge le nombre d'arguments
-    cmp rax, 1              ; Vérifie s'il y a au moins un argument
-    jle no_argument         ; S'il n'y en a pas, termine avec code d'erreur
+    mov rax, [rsp]
+    cmp rax, 1
+    jle no_argument
 
-    mov rsi, [rsp + 16]     ; Charge l'adresse du premier argument (argv[1])
-    call string_length      ; Calcule la longueur de la chaîne
+    mov rsi, [rsp + 16]
+    call string_length
 
-    mov rdx, rax            ; Place la longueur de la chaîne dans rdx
-    mov rax, 1              ; Syscall pour write
-    mov rdi, 1              ; Descripteur de fichier pour stdout
-    syscall                 ; Appel système pour écrire la chaîne
+    mov rdx, rax
+    mov rax, 1
+    mov rdi, 1
+    syscall
 
-    xor rdi, rdi            ; Code de sortie 0
-    mov rax, 60             ; Syscall pour exit
-    syscall                 ; Quitte le programme
+    xor rdi, rdi
+    mov rax, 60
+    syscall
 
 no_argument:
-    mov rdi, 1              ; Code de sortie 1 si aucun argument
-    mov rax, 60             ; Syscall pour exit
-    syscall                 ; Quitte le programme
+    mov rdi, 1
+    mov rax, 60
+    syscall
 
-; Calcule la longueur de la chaîne ASCII terminée par zéro
-; Entrée: rsi = adresse de la chaîne
-; Sortie: rax = longueur
 string_length:
-    xor rax, rax            ; RAZ le compteur de longueur
+    xor rax, rax
 find_end:
-    cmp byte [rsi + rax], 0 ; Compare avec le caractère nul
-    je end_found            ; Si trouvé, fin de boucle
-    inc rax                 ; Sinon, incrémente le compteur
-    jmp find_end            ; Répète la boucle
+    cmp byte [rsi + rax], 0
+    je end_found
+    inc rax
+    jmp find_end
 end_found:
-    ret                     ; Retourne avec la longueur dans rax
+    ret
