ba1b819fdd76ea9180ae5f8d461515d92c464a97
Saygel <jmicu@myges.fr>
2025-01-14 10:55:47 +0100

    asm05

--- asm05/asm05.s
+++ asm05/asm05.s
@@ -2,25 +2,38 @@ section .text
 global _start
 
 _start:
-    ; Vérifier si au moins un argument a été passé
-    mov rax, [rsp]          ; nombre d'arguments dans argc
-    cmp rax, 1              ; argc doit être > 1 pour avoir au moins un argument
-    jle no_argument         ; sauter si aucun argument n'est passé
+    mov rax, [rsp]        
+    cmp rax, 1             
+    jle no_argument      
 
-    ; Préparer l'appel système write pour afficher l'argument
-    mov rax, 1              ; syscall number for write
-    mov rdi, 1              ; file descriptor 1 (stdout)
-    mov rsi, [rsp + 16]     ; adresse du premier argument (argv[1])
-    mov rdx, [rsp + 24]     ; taille du premier argument (argv[1] length)
-    syscall                 ; effectuer l'appel système
 
-    ; Terminer le programme avec le code de retour 0
-    xor rdi, rdi            ; code de retour 0
-    mov rax, 60             ; syscall number for exit
-    syscall                 ; effectuer l'appel système
+    mov rsi, [rsp + 16]    
+
+
+    call string_length    
+
+ 
+    mov rax, 1           
+    mov rdi, 1            
+    mov rdx, rax        
+    syscall             
+
+
+    xor rdi, rdi     
+    mov rax, 60         
+    syscall               
 
 no_argument:
-    ; Terminer le programme avec le code de retour 1 s'il n'y a pas d'argument
-    mov rdi, 1              ; code de retour 1
-    mov rax, 60             ; syscall number for exit
-    syscall                 ; effectuer l'appel système
+    mov rdi, 1             
+    mov rax, 60            
+    syscall                
+
+string_length:
+    xor rax, rax           
+count_loop:
+    cmp byte [rsi + rax], 0 
+    je end_count           
+    inc rax                
+    jmp count_loop         
+end_count:
+    ret                    
